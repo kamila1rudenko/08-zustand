4a3d8aa1a7b933f48ea55612196397d15e90f801
Kamila <kamillkaa95@gmail.com>
2025-08-20 11:30:37 +0300

    css header fix

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -5,13 +5,13 @@ import Link from 'next/link';
 const Header = () => {
   return (
     <header className={css.header}>
-      <Link href="/" aria-label="Home">
+      <Link className={css.linkHeader} href="/" aria-label="Home">
         NoteHub
       </Link>
       <nav aria-label="Main Navigation">
         <ul className={css.navigation}>
           <li>
-            <Link className={css.link} href="/">Home</Link>
+            <Link className={css.linkHeader} href="/">Home</Link>
           </li>
           <li>
             <TagsMenu />
